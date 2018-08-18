3bccc5d0484cde98efa3e9f65b484c8a30aeddd4
sneurlax <sneurlax@gmail.com>
2018-08-17 18:05:06 -0700

    tests also check that status is 'OK'

--- test/index_test.js
+++ test/index_test.js
@@ -4,30 +4,98 @@ var Monero = require('../index.js');
 describe('remote nodes', () => {
   const daemons = require('../lib/remote-daemons.json');
 
-  for (let i in daemons) {
-    let daemon = daemons[i];
+  for (let network in daemons) {
+    for (let i in daemons[network]) {
+      // TODO time each connection; store fastest for use later
+      let daemon = daemons[network][i];
 
-    it(`should connect to daemon at ${daemon['hostname']}`, done => {
-      let daemonRPC = new Monero.daemonRPC(daemon) // Check if remote daemon is available
-      .then(daemon => {
-        daemon.should.be.a.Object();
-        // monero-wallet-rpcremote daemon available
-        done();
+      it(`should connect to ${network} daemon at ${daemon['hostname']}`, done => {
+        let daemonRPC = new Monero.daemonRPC(daemon) // Check if remote daemon is available
+        .then(daemon => {
+          daemon.should.be.a.Object();
+          // daemon available
+        })
+        .catch(err => {
+          // daemon unavailable
+        })
+        .then(done, done);
       })
-      .catch(err => {
-        // remote daemon unavailable
-      })
-      .then(done, done);
-    })
-    .timeout(5000);
+      .timeout(5000);
+    }
   }
 });
 
 describe('daemonRPC constructor', () => {
-  // TODO test multiple Monero networks (mainnet, testnet, stagenet, etc.)
-  it('should autoconnect to daemon', done => {
+  // TODO test one at a time?
+  it('should autoconnect to mainnet daemon', done => {
+    var daemonRPC = new Monero.daemonRPC({ autoconnect: true, random: true, network: 'mainnet' })
+    .then(daemon => {
+      daemon.should.be.a.Object();
+
+      daemon.get_info()
+      .then(result => {
+        result.should.be.a.Object();
+        result.status.should.be.a.String();
+        result.status.should.be.equal('OK');
+        result.mainnet.should.be.a.Boolean();
+        result.mainnet.should.be.equal(true);
+      });
+    })
+    .catch(error => {
+      // TODO handle error
+    })
+    .then(done, done);
+  })
+  .timeout(5000);
+
+  // TODO allow for test to pass if no local testnet daemon available and no remote testnet nodes listed
+  it('should autoconnect to testnet daemon', done => {
+    var daemonRPC = new Monero.daemonRPC({ autoconnect: true, random: true, network: 'testnet' })
+    .then(daemon => {
+      daemon.should.be.a.Object();
+
+      daemon.get_info()
+      .then(result => {
+        result.should.be.a.Object();
+        result.status.should.be.a.String();
+        result.status.should.be.equal('OK');
+        result.testnet.should.be.a.Boolean();
+        result.testnet.should.be.equal(true);
+      });
+    })
+    .catch(error => {
+      // TODO handle error
+    })
+    .then(done, done);
+  })
+  .timeout(5000);
+
+  // TODO allow for test to pass if no local stagenet daemon available and no remote stagenet nodes listed
+  it('should autoconnect to stagenet daemon', done => {
+    var daemonRPC = new Monero.daemonRPC({ autoconnect: true, random: true, network: 'stagenet' })
+    .then(daemon => {
+      daemon.should.be.a.Object();
+
+      daemon.get_info()
+      .then(result => {
+        result.should.be.a.Object();
+        result.status.should.be.a.String();
+        result.status.should.be.equal('OK');
+        result.stagenet.should.be.a.Boolean();
+        result.stagenet.should.be.equal(true);
+      });
+    })
+    .catch(error => {
+      // TODO handle error
+    })
+    .then(done, done);
+  })
+  .timeout(5000);
+
+  // TODO connect to fastest daemon from test above
+  it('should connect to daemon', done => {
     var daemonRPC = new Monero.daemonRPC({ autoconnect: true, random: true })
-    .then(daemon => { // TODO add type for daemon
+    .then(daemon => {
       daemon.should.be.a.Object();
 
       daemonRPC = daemon; // Store daemon interface in global variable
@@ -45,6 +113,8 @@ describe('daemonRPC constructor', () => {
                 }
               } else {
                 result.should.be.a.Object();
+                result.status.should.be.a.String();
+                result.status.should.be.equal('OK');
               }
             })
             .then(done, done);
@@ -55,6 +125,7 @@ describe('daemonRPC constructor', () => {
           it('should return the block 42069\'s hash', done => {
             daemonRPC.on_getblockhash(42069)
             .then(result => {
+              // Why no status string?
               result.should.be.a.String();
               result.should.be.equal('4aff3d3f2a939ddf7c971b57b428841bccde6dc937404346398c4133ba37359b');
             })
@@ -66,9 +137,11 @@ describe('daemonRPC constructor', () => {
           it('should return a block template', done => {
             daemonRPC.getblocktemplate('44AFFq5kSiGBoZ4NMDwYtN18obc8AemS33DBLWs3H7otXft3XjrpDtQGv7SqSsaBYBb98uNbr2VBBEt7f2wfn3RVGQBEP3A', 255)
             .then(result => {
-              result.should.be.a.Object();
-              result.blocktemplate_blob.should.be.a.String();
-              result.height.should.be.a.Number();
+              result.should.be.a.String();
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
+              JSON.parse(result).blocktemplate_blob.should.be.a.String();
+              JSON.parse(result).height.should.be.a.Number();
             })
             .then(done, done);
           });
@@ -81,6 +154,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getlastblockheader()
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK'); // TODO allow for 'BUSY'
               result.block_header.should.be.a.Object();
               result.block_header.hash.should.be.a.String();
               result.block_header.nonce.should.be.a.Number();
@@ -94,6 +169,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getblockheaderbyhash('3b380d7dac4fe41864fe2fc4b7ef7b2e70575d4d847d1f590550b88dc2e9fdf9')
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.block_header.should.be.a.Object();
               result.block_header.height.should.be.a.Number();
               result.block_header.height.should.be.equal(31337);
@@ -108,6 +185,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getblockheaderbyheight(1234567)
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.block_header.should.be.a.Object();
               result.block_header.hash.should.be.a.String();
               result.block_header.hash.should.be.equal('f093439d0dd48010a22fdb615a659e22738a10991871b5dc2335efa69008a8cd');
@@ -122,6 +201,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getblock_by_hash('5ab00b5f78f731a64ace5558c909cf2d74021d41218021806845197107c6709a')
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.blob.should.be.a.String();
               result.block_header.should.be.a.Object();
               result.block_header.height.should.be.a.Number();
@@ -137,6 +218,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getblock_by_height(202020)
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.blob.should.be.a.String();
               result.block_header.should.be.a.Object();
               result.block_header.hash.should.be.a.String();
@@ -153,6 +236,8 @@ describe('daemonRPC constructor', () => {
         //     daemonRPC.get_connections()
         //     .then(result => {
         //       result.should.be.a.Object();
+        //       result.status.should.be.a.String();
+        //       result.status.should.be.equal('OK');
         //       console.log(result)
         //       // result.blob.should.be.a.String();
         //       // result.block_header.should.be.a.Object();
@@ -169,6 +254,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.get_outs([{ index: 77777 }])
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.outs.should.be.a.Array();
               result.outs[0].should.be.a.Object();
               result.outs[0].key.should.be.a.String();
@@ -182,6 +269,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.hard_fork_info()
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.earliest_height.should.be.a.Number();
               result.version.should.be.a.Number();
             })
@@ -194,6 +283,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.getheight()
             .then(result => {
               result.should.be.a.String();
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
               JSON.parse(result).should.be.a.Object();
               JSON.parse(result).height.should.be.a.Number();
             })
@@ -206,6 +297,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.gettransactions(['99a992675f1204ea114e1ad14f2e622554f46b3a9cacc91c6255b789906269d5'])
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.txs.should.be.a.Array();
               result.txs[0].should.be.a.Object();
               result.txs[0].block_height.should.be.a.Number();
@@ -221,7 +314,11 @@ describe('daemonRPC constructor', () => {
             .then(result => {
               result.should.be.a.String();
               JSON.parse(result).should.be.a.Object();
-              JSON.parse(result).blks_hashes.should.be.a.Array();
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
+              if ('blks_hashes' in JSON.parse(result)) {
+                JSON.parse(result).blks_hashes.should.be.a.Array();
+              }
             })
             .then(done, done);
           });
@@ -232,6 +329,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.is_key_image_spent(['8d1bd8181bf7d857bdb281e0153d84cd55a3fcaa57c3e570f4a49f935850b5e3'])
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.spent_status.should.be.a.Array();
               result.spent_status[0].should.be.a.Number();
               result.spent_status[0].should.be.equal(1);
@@ -257,6 +356,8 @@ describe('daemonRPC constructor', () => {
               result.should.be.a.String();
               // TODO parse JSON
               // JSON.parse(result).should.be.a.Object();
+              // JSON.parse(result).status.should.be.a.String();
+              // JSON.parse(result).status.should.be.equal('OK');
               // JSON.parse(result).key.should.be.a.String();
             })
             .then(done, done);
@@ -270,6 +371,8 @@ describe('daemonRPC constructor', () => {
               result.should.be.a.String();
               // TODO parse JSON
               // JSON.parse(result).should.be.a.Object();
+              // JSON.parse(result).status.should.be.a.String();
+              // JSON.parse(result).status.should.be.equal('OK');
               // JSON.parse(result).pool_stats.should.be.a.Object();
               // JSON.parse(result).pool_stats.txs_total.should.be.a.Number();
               // JSON.parse(result).pool_stats.histo.should.be.a.String();
@@ -283,6 +386,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.get_info()
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.tx_count.should.be.a.Number();
               result.status.should.be.a.String();
             })
@@ -296,6 +401,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.get_limit()
             .then(result => {
               JSON.parse(result).should.be.a.Object();
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
               JSON.parse(result).limit_down.should.be.a.Number();
             })
             .then(done, done);
@@ -308,6 +415,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.set_limit(-1, -1)
             .then(result => {
               JSON.parse(result).should.be.a.Object();
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
               JSON.parse(result).limit_down.should.be.a.Number();
             })
             .then(done, done);
@@ -320,7 +429,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.out_peers(10000)
             .then(result => {
               JSON.parse(result).should.be.a.Object();
-              JSON.parse(result).status.should.be.a.equal('OK');
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
             })
             .then(done, done);
           });
@@ -332,7 +442,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.in_peers(10000)
             .then(result => {
               JSON.parse(result).should.be.a.Object();
-              JSON.parse(result).status.should.be.a.equal('OK');
+              JSON.parse(result).status.should.be.a.String();
+              JSON.parse(result).status.should.be.equal('OK');
             })
             .then(done, done);
           });
@@ -343,6 +454,8 @@ describe('daemonRPC constructor', () => {
             daemonRPC.get_outs([{ index: 77777 }])
             .then(result => {
               result.should.be.a.Object();
+              result.status.should.be.a.String();
+              result.status.should.be.equal('OK');
               result.outs.should.be.a.Array();
               result.outs[0].should.be.a.Object();
               result.outs[0].key.should.be.a.String();
@@ -358,7 +471,7 @@ describe('daemonRPC constructor', () => {
         //     daemonRPC.stop_daemon()
         //     .then(result => {
         //       JSON.parse(result).should.be.a.Object();
-        //       JSON.parse(result).status.should.be.a.equal('OK');
+        //       JSON.parse(result).status.should.be.equal('OK');
 
         //       // TODO restart daemon
         //     })
